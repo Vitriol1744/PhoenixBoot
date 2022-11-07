26e01808d57666d4d4d7f45617bec045312f9c84
Vitriol1744 <vitriol1744@gmail.com>
2022-11-07 22:51:07 +0100

    Some tests

--- src/stage2/main.cpp
+++ src/stage2/main.cpp
@@ -1,10 +1,11 @@
 #include <cstddef>
 #include <cstdint>
 
-inline void* operator new(std::size_t, void* p)     noexcept { return p; }
-inline void* operator new[](std::size_t, void* p)   noexcept { return p; }
-inline void  operator delete(void*, void*)          noexcept { };
-inline void  operator delete[](void*, void*)        noexcept { };
+#include "arch/x86/x86.h"
+
+#include "drivers/TextModeTerminal.hpp"
+
+#include "lib/libc.hpp"
 
 extern uint8_t* __bss_start[];
 extern uint8_t* __bss_end[];
@@ -14,6 +15,8 @@ using ConstructorFunction = void(*)();
 extern ConstructorFunction __init_array_start[];
 extern ConstructorFunction __init_array_end[];
 
+
+//TODO: Most of this stuff is temporary and just for tests, so don't judge this code
 class A
 {
     public:
@@ -54,6 +57,11 @@ extern "C"
             destructors[i].destructor(destructors[i].objptr);
         }
     }
+
+    void __cxa_pure_virtual()
+    {
+        // Do nothing or print an error message.
+    }
 }
 
 extern "C" __attribute__((section(".entry"))) void Stage2Main(uint8_t bootDrive, size_t stage2Size)
@@ -66,7 +74,17 @@ extern "C" __attribute__((section(".entry"))) void Stage2Main(uint8_t bootDrive,
     // Call global constructors
     for (ConstructorFunction* f = __init_array_start; f < __init_array_end; f++) (*f)();
 
+    TextModeTerminal::Initialize();
+    //TODO: Render some cool ASCII art
+                           
     *(long long*)0xb8000 = 0x12591241124b124f;
+    Terminal::Get()->SetY(1);
+    Terminal::Get()->SetColor(TerminalColor::Cyan, TerminalColor::Black);
+
+    char buffer[20];
+    itoa(146, buffer, 16);
+    Terminal::Get()->PrintString(buffer);
+    printf("some_value: %c%c%x%x%r%d", 'a', 'b', 0xf, 0xf, "123", 3);
 
     __cxa_finalize();
     __asm__("hlt");
