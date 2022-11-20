c76be4fc70240953b74b2857ffc087e3316c1517
Vitriol1744 <vitriol1744@gmail.com>
2022-11-20 13:19:05 +0100

    Add ReadAll

--- src/stage2/Filesystem/File.hpp
+++ src/stage2/Filesystem/File.hpp
@@ -12,6 +12,7 @@ class File
 
         virtual bool Open(const char* filename) = 0;
         virtual bool Read(void* buffer, uint64_t bytes) = 0;
+        bool ReadAll(void* buffer) { return Read(buffer, size); }
 
         inline uint64_t GetSize() const { return size; }
 
